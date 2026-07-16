6eb459afde89f9a7d6ed2b6f061ad3d49f507e9c
Karabo-Merinah <skosanamerinah@gmail.com>
2026-07-16 08:44:27 +0200

    Added Collection media queries

--- src/assets/Components/Footer/Footer.tsx
+++ src/assets/Components/Footer/Footer.tsx
@@ -3,6 +3,7 @@ import {Text} from '../Text/Text'
 import {FaFacebook,FaInstagram,FaPinterest} from 'react-icons/fa'
 import footerCard from '../../pictures/footer_card.png'
 import footerImg from '../../pictures/footer.jpg'
+
 export const Footer = () => {
   return (
    <footer>
@@ -12,13 +13,13 @@ export const Footer = () => {
     <img  src={footerCard}></img>
     </div>
      <div className={styles['social-links']}>
-    <Text variant={'h2'} style={{color:'#ECCFC8',fontSize:25,fontFamily:"Aguafina Script",fontStyle:'italic',textAlign:'left'}} >Palisirie Follow us on this socials</Text>
+    <Text variant={'h2'} style={{color:'#963949',fontSize:25,fontFamily:"Aguafina Script",fontStyle:'italic',textAlign:'left'}} >Palisirie Follow us on this socials</Text>
     <div className={styles['socials-align']}>
      <a className={styles['facebook-icon']} href="https://www.facebook.com" ><FaFacebook/></a>
      <a className={styles['instagram-icon']} href="https://www.instagram.com"><FaInstagram/></a>
      <a className={styles['pinterest-icon']} href="https://www.pinterest.com"><FaPinterest/></a>
      </div>
-     <Text variant={'h3'} style={{fontFamily: "'Cormorant Garamond', serif",textAlign:'right',color:'rgb(20,20,20)'}}>Thanks for the support</Text>
+     <Text variant={'h3'} style={{fontFamily: "'Cormorant Garamond', serif",textAlign:'right',color:'rgb(20,20,20)',fontSize:10}}>Thanks for the support</Text>
       </div>
       </div>
    </footer>
